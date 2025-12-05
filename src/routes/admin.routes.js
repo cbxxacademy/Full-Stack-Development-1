ab20143f6c5ecdc778c5832c2c82ccbbb59156aa
Etienne Gonthier Lapointe <etienne.gonthier-lapointe@codeboxx.biz>
2025-12-05 10:16:18 -0500

    chore: remove old full-version project files

--- src/routes/admin.routes.js
+++
@@ -1,9 +0,0 @@
-const AdminController = require('../features/admin/admin.controller');
-
-const registerAdminRoutes = (app) => {
-  app.get('/email-list', AdminController.emailList);
-
-  app.get('/region-avg', AdminController.regionAverage);
-}
-
-module.exports = {registerAdminRoutes};
--- src/routes/public.routes.js
+++
@@ -1,9 +0,0 @@
-const PublicController = require('../features/public/public.controller');
-
-const registerPublicRoutes = (app) => {
-  app.post('/contact', PublicController.contactUs);
-
-  app.get('/calc-residential', PublicController.calculateResidentialQuote);
-}
-
-module.exports = {registerPublicRoutes};
